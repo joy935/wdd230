a04e7cfe273f8d9170c79ab927c7786ee84f9664
joy935 <tupaiv@gmail.com>
2024-02-07 11:42:18 +0100

    added style dark mode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,1 +1,14 @@
-const darkMode = document.querySelector('#mode');
+const darkMode = document.querySelector('.switch input[type="checkbox"]');
+const body = document.querySelector('body');
+const boxevent = document.querySelector('.events-box');
+
+darkMode.addEventListener('change', () => {
+    if (darkMode.checked) {
+		body.classList.add('dark-style');
+        boxevent.style.background = '#333333';
+	} 
+    else {
+        body.classList.remove('dark-style');
+        boxevent.style.background = '#OB261A';
+    }
+});
