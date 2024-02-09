cb9c10055ab25a7b592546cedea41d04cacdecf5
joy935 <tupaiv@gmail.com>
2024-02-09 10:36:01 +0100

    fixed contrast colors

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,14 +1,17 @@
 const darkMode = document.querySelector('.switch input[type="checkbox"]');
 const body = document.querySelector('body');
 const boxevent = document.querySelector('.events-box');
+const contact = document.querySelector('.contact');
 
 darkMode.addEventListener('change', () => {
     if (darkMode.checked) {
 		body.classList.add('dark-style');
         boxevent.style.background = '#333333';
+        contact.style.background = '#333333';
 	} 
     else {
         body.classList.remove('dark-style');
         boxevent.style.background = '#E9F1EE';
+        contact.style.background = '#274F32';
     }
 });
