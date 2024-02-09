4a2e9b9a61a5c85a594be6f723584f14db660fef
joy935 <tupaiv@gmail.com>
2024-02-09 10:43:33 +0100

    added dark styles for contact

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,17 +1,21 @@
 const darkMode = document.querySelector('.switch input[type="checkbox"]');
 const body = document.querySelector('body');
 const boxevent = document.querySelector('.events-box');
-const contact = document.querySelector('.contact');
+const contacts = document.querySelectorAll('.contact');
 
 darkMode.addEventListener('change', () => {
     if (darkMode.checked) {
 		body.classList.add('dark-style');
         boxevent.style.background = '#333333';
-        contact.style.color = '#000';
+        contacts.forEach(contact => {
+            contact.style.color = 'white';
+        });
 	} 
     else {
         body.classList.remove('dark-style');
         boxevent.style.background = '#E9F1EE';
-        contact.style.background = '#274F32';
+        contacts.forEach(contact => {
+            contact.style.color = '#274F32';
+        });
     }
 });
