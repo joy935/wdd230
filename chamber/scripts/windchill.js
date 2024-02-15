210959a88c0a9b7c364992f06a0cc3ed8495daf9
joy935 <tupaiv@gmail.com>
2024-02-15 15:38:12 +0100

    added display temperature

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -5,11 +5,21 @@ const getWindchill = async () => {
     try {
         const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/brugg%20aargau%20switzerland?unitGroup=us&key=TL4E7JG8UXWJPWVKL7GVJ29U2&contentType=json")
         infoList = await response.json()
+        displayTemperature(infoList)
         displayWindchill(infoList)
     } catch (error) {
         console.error("Error fetching data: ", error)
     }
 };
+
+const displayTemperature = (infoList) => {
+    if (infoList && infoList.days && infoList.days.length > 0) {
+        const currentDay = infoList.days[0];
+        document.getElementById("temperature").innerHTML = `Temperature: ${currentDay.temp}°F`
+    } else {
+        document.getElementById("temperature").innerHTML = "Temperature: N/A"
+    }
+}
 
 const displayWindchill= (infoList) => {
     if (infoList && infoList.days && infoList.days.length > 0) {
