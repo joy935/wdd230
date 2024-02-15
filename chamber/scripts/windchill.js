4145444194b93859ceec8cbbd0377a86577ad7ce
joy935 <tupaiv@gmail.com>
2024-02-15 15:17:03 +0100

    fixed js

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,21 +1,28 @@
 
+const windchill = document.getElementById("windchill");
+let infoList = [];
+
 const getWindchill = async () => {
     try {
         const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/brugg%20aargau%20switzerland?unitGroup=us&key=TL4E7JG8UXWJPWVKL7GVJ29U2&contentType=json")
         infoList = await response.json()
-        displayInfo(infoList)
-    } 
-    catch (error) {
-        console.error("Error fetching emoji data: ", error)
+        displayWindchill(infoList)
+    } catch (error) {
+        console.error("Error fetching data: ", error)
     }
 };
 
 const displayWindchill= (infoList) => {
-    const temperature = infoList.days[0].temp
-    const windspeed = infoList.days[0].windspeed
-    const windchill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16)
-    document.getElementById("windchill").innerHTML = `Windchill: ${windchill.toFixed(2)}°F`
+    if (infoList && infoList.days && infoList.days.length > 0) {
+        const currentDay = infoList.days[0];
+        const temperature = currentDay.temp
+        const windspeed = currentDay.windspeed
+        const windchill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16)
+        document.getElementById("temperature").innerHTML = `Temperature: ${temperature}°F`
+        document.getElementById("windchill").innerHTML = `Windchill: ${windchill.toFixed(2)}°F`
+    } else {
+        document.getElementById("windchill").innerHTML = "Windchill: N/A"
+    }
 };
 
-
-displayWindchill();
+getWindchill();
