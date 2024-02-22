1d0b56dfa52bc2269249c685d2230f0010d80061
joy935 <tupaiv@gmail.com>
2024-02-22 10:24:04 +0100

    added comments

--- scripts/getvisits.js
+++ scripts/getvisits.js
@@ -1,13 +1,16 @@
+
 const visitDisplay = document.getElementById('visits');
 
+// Get the number of visits from local storage
 let numVisits = Number(localStorage.getItem('numVisits-list')) || 0;
 
+// Display the number of visits if there are any, otherwise display a welcome message
 if (numVisits != 0) {
     visitDisplay.innerHTML = `Number of Visits: ${numVisits}`;
 } else {
     visitDisplay.innerHTML = `This is your first visit, welcome!`;
 }
 
+// Increment the number of visits and save it to local storage
 numVisits++;
-
 localStorage.setItem('numVisits-list', numVisits);
