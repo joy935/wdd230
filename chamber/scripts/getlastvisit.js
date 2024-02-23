b68dd44bc275acaa422a19137ba71cd30b9b1f66
joy935 <tupaiv@gmail.com>
2024-02-23 17:09:18 +0100

    changed cdt

--- chamber/scripts/getlastvisit.js
+++ chamber/scripts/getlastvisit.js
@@ -9,14 +9,20 @@ let lastVisit = Number(localStorage.getItem('lastVisitTime')) || 0;
 const oneDayInMs = 1000 * 60 * 60 * 24;
 let daysSinceLastVisit = Math.floor((currentTime - lastVisit) / oneDayInMs);
 
-console.log(daysSinceLastVisit);
+const minsSinceLastVisit = ((currentTime - lastVisit) / (1000 * 60));
 
-if (daysSinceLastVisit == 0) {
-    visitDisplay.innerHTML = `Welcome! Let us know if you have any questions.`;
-} else if (daysSinceLastVisit < 1) {
+console.log(`Days: ${daysSinceLastVisit}`);
+console.log(`Mins: ${minsSinceLastVisit}`);
+console.log(`Last Visit: ${lastVisit}`);    
+console.log(`Current Time: ${currentTime}`);
+
+if (daysSinceLastVisit < 1 && minsSinceLastVisit > 0) {
     visitDisplay.innerHTML = `Back so soon! Awesome!`;
+} else if (daysSinceLastVisit == 0) {
+    visitDisplay.innerHTML = `Welcome! Let us know if you have any questions.`;
 } else if (daysSinceLastVisit == 1){ 
-    visitDisplay.innerHTML = `You last visited 1 day ago!`;
+        visitDisplay.innerHTML = `You last visited 1 day ago!`;
+    
 } else {
     visitDisplay.innerHTML = `You last visited ${daysSinceLastVisit} days ago!`;
 }
