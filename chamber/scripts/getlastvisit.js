cc137d2e002f033fce702e8a61b76411de878469
joy935 <tupaiv@gmail.com>
2024-02-23 17:32:35 +0100

    fixed conditions

--- chamber/scripts/getlastvisit.js
+++ chamber/scripts/getlastvisit.js
@@ -9,17 +9,21 @@ let lastVisit = Number(localStorage.getItem('lastVisitTime')) || 0;
 const oneDayInMs = 1000 * 60 * 60 * 24;
 let daysSinceLastVisit = Math.floor((currentTime - lastVisit) / oneDayInMs);
 
-const minsSinceLastVisit = ((currentTime - lastVisit) / (1000 * 60));
+const minsSinceLastVisit = Math.ceil((currentTime - lastVisit) / (1000 * 60));
 
-if (daysSinceLastVisit < 1 && minsSinceLastVisit > 0) {
-    visitDisplay.innerHTML = `Back so soon! Awesome!`;
-} else if (daysSinceLastVisit == 0) {
+if (daysSinceLastVisit == 0 && minsSinceLastVisit == 0) {
     visitDisplay.innerHTML = `Welcome! Let us know if you have any questions.`;
+} else if (daysSinceLastVisit == 0 && minsSinceLastVisit != 0) {
+    visitDisplay.innerHTML = `Back so soon! Awesome!`;
 } else if (daysSinceLastVisit == 1){ 
         visitDisplay.innerHTML = `You last visited 1 day ago!`;
-    
 } else {
     visitDisplay.innerHTML = `You last visited ${daysSinceLastVisit} days ago!`;
 }
 
 localStorage.setItem('lastVisitTime', currentTime);
+
+console.log('Last visit time:', lastVisit);
+console.log('Current time:', currentTime);
+console.log('Days since last visit:', daysSinceLastVisit);
+console.log('Mins since last visit:', minsSinceLastVisit);
