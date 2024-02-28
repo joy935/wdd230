af7c2e419f83763c3c75cf526107aa790cd22fe3
joy935 <tupaiv@gmail.com>
2024-02-28 15:50:37 +0100

    moved js files

--- lesson08/scripts/responsivemenu.js
+++
@@ -1,7 +0,0 @@
-const hamButton = document.querySelector('#menu');
-const mainnav = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    mainnav.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
