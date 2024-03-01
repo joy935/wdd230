6fd1430f29c2b466ec3d0ad65e80d870425e675f
joy935 <tupaiv@gmail.com>
2024-03-01 11:12:00 +0100

    added comments

--- chamber/scripts/getrecord.js
+++ chamber/scripts/getrecord.js
@@ -1,5 +1,8 @@
+// get the current date and time
 const currentDate = new Date();
 const currentTime = currentDate.getTime();
+
 const currentRecordDate = document.getElementById('date');
 
+// Set the date input field to the current date
 currentRecordDate.value = currentTime;
