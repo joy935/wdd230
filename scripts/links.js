bf01cd129ff734108af8f28a05bb608695d18caf
joy935 <tupaiv@gmail.com>
2024-03-05 10:32:24 +0100

    linked js

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,19 @@
 const baseURL = "https://joy935.github.io/wdd230/";
 const linksURL =  "https://joy935.github.io/wdd230/data/links.json";
+
+const links = document.querySelector('#links');
+
+let linkList = [];
+
+const getLinks = async () => {
+    try {
+        const response = await fetch(linksURL);
+        linkList = await response.json();
+        console.table(linkList);
+        // displayLinks(linkList);
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+    }
+};
+
+getLinks();
