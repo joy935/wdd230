27facf3256b70a3ea5c3419ea215f6b4d5431954
joy935 <tupaiv@gmail.com>
2024-03-05 10:34:47 +0100

    changed variables

--- scripts/links.js
+++ scripts/links.js
@@ -3,13 +3,13 @@ const linksURL =  "https://joy935.github.io/wdd230/data/links.json";
 
 const links = document.querySelector('#links');
 
-let linkList = [];
+let data = [];
 
 const getLinks = async () => {
     try {
         const response = await fetch(linksURL);
-        linkList = await response.json();
-        console.table(linkList);
+        data = await response.json();
+        console.table(data);
         // displayLinks(linkList);
     } catch (error) {
         console.error("Error fetching data: ", error);
