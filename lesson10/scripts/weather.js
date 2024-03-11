5d80fb9474e420fbdcd6518f10ee20dc39bb2dcf
joy935 <tupaiv@gmail.com>
2024-03-11 09:47:04 +0100

    added url for api

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,3 +1,5 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=710bb94f8ac1a0695f7197b282e4ba48";
