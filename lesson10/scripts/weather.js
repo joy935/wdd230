0d163eda15c9fbe04d3e9a1be07f4a754f2cdd9b
joy935 <tupaiv@gmail.com>
2024-03-11 14:33:38 +0100

    completed capitalize and round f

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,5 +1,5 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
+const currentTemp = document.getElementById('current-temp');
+const weatherIcon = document.getElementById('weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=710bb94f8ac1a0695f7197b282e4ba48";
@@ -11,6 +11,7 @@ const apiFetch = async () => {
         if (response.ok) {
             const data = await response.json();
             // console.log(data); // test the data
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -19,7 +20,24 @@ const apiFetch = async () => {
     }
 };
 
+const capitalize = (string) => {
+    return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+};
 
+const roundToWhole = (number) => {
+    return Math.round(number);
+};
 
+function displayResults(data) {
+    let temp = data.main.temp;
+    let desc = data.weather[0].description;
+    let icon = data.weather[0].icon;
+
+    currentTemp.innerHTML = `${roundToWhole(temp)}&deg;F`;
+    captionDesc.innerHTML = capitalize(desc);
+    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('loading', 'lazy');
+    weatherIcon.src = `http://openweathermap.org/img/wn/${icon}.png`;
+};
 
 apiFetch();
