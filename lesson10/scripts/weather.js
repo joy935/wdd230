8ce037f6e73013d351145e7b55e564545663f391
joy935 <tupaiv@gmail.com>
2024-03-11 12:57:23 +0100

    created apiFetch f

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,4 +2,17 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=710bb94f8ac1a0695f7197b282e4ba48";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=710bb94f8ac1a0695f7197b282e4ba48";
+
+const apiFetch = async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data);
+    }
+    catch (error) {
+        console.error("Error fetching data: ", error);
+    }
+};
+
+apiFetch();
