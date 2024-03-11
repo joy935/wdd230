5cb20d1f4df80672ab0c0ec2deae79d4a41f731b
joy935 <tupaiv@gmail.com>
2024-03-11 13:48:40 +0100

    fixed error

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -5,6 +5,7 @@ const captionDesc = document.querySelector('figcaption');
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=710bb94f8ac1a0695f7197b282e4ba48";
 
 const apiFetch = async () => {
+    try {
         const response = await fetch(url);
 
         if (response.ok) {
@@ -13,6 +14,9 @@ const apiFetch = async () => {
         } else {
             throw new Error(await response.text());
         }
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+    }
 };
 
 apiFetch();
