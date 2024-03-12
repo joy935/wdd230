74418d735715d0f41203c441dc4396a4d2ee7d88
joy935 <tupaiv@gmail.com>
2024-03-12 09:02:40 +0100

    created getWeather f

--- chamber/scripts/getweather.js
+++ chamber/scripts/getweather.js
@@ -4,3 +4,17 @@ const weatherDesc = document.querySelector("figcaption");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=47.48&lon=8.21&units=imperial&appid=710bb94f8ac1a0695f7197b282e4ba48";
 
+const getWeather = async () => {
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const data = await response.json()
+            displayWeather(data)
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.error("Error fetching data: ", error)
+    }
+};
