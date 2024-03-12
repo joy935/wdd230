be8c0a0db5b8873a590f0576aba7b03582a5f8d7
joy935 <tupaiv@gmail.com>
2024-03-12 13:30:21 +0100

    fixed conditions

--- chamber/scripts/getbanner.js
+++ chamber/scripts/getbanner.js
@@ -4,10 +4,10 @@ const bannerMsg = document.getElementById('banner-msg');
 const currentDate = new Date();
 const currentDay = currentDate.getDay();
 
-if (currentDay === 1 || currentDay === 2 || currentDay === 3) {
+if (currentDay === 1 || currentDay ==2 || currentDay === 3) {
   banner.style.display = 'block';
   bannerMsg.textContent = `Save the Date: The Chamber of Commerce will have a Meet and Greet on Wednesday at 7:00 p.m.`;
-  console.log(bannerMsg);
 } else {
-  banner.style.display = 'Welcome to the Chamber of Commerce!';
+    banner.style.display = 'block';
+    bannerMsg.textContent = 'Welcome to the Chamber of Commerce!';
 }
