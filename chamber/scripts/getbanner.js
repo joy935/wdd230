13222f86731928735dca963fc6520430ca1d1721
joy935 <tupaiv@gmail.com>
2024-03-12 13:10:35 +0100

    created cdt banner

--- chamber/scripts/getbanner.js
+++ chamber/scripts/getbanner.js
@@ -1,2 +1,13 @@
 const banner = document.getElementById('banner');
 const bannerMsg = document.getElementById('banner-msg');
+
+const currentDate = new Date();
+const currentDay = currentDate.getDay();
+
+if (currentDay === 1 || currentDay === 2 || currentDay === 3) {
+  banner.style.display = 'block';
+  bannerMsg.textContent = `Save the Date: The Chamber of Commerce will have a Meet and Greet on Wednesday at 7:00 p.m.`;
+  console.log(bannerMsg);
+} else {
+  banner.style.display = 'Welcome to the Chamber of Commerce!';
+}
