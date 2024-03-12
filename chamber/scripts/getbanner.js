fcf362bdceee0597057548d938f5eecc0c28f203
joy935 <tupaiv@gmail.com>
2024-03-12 15:23:40 +0100

    changed banner display

--- chamber/scripts/getbanner.js
+++ chamber/scripts/getbanner.js
@@ -4,10 +4,9 @@ const bannerMsg = document.getElementById('banner-msg');
 const currentDate = new Date();
 const currentDay = currentDate.getDay();
 
-if (currentDay === 1 || currentDay ==2 || currentDay === 3) {
+if (currentDay === 1 || currentDay == 2 || currentDay === 3) {
   banner.style.display = 'block';
   bannerMsg.textContent = `Save the Date: The Chamber of Commerce will have a Meet and Greet on Wednesday at 7:00 p.m.`;
 } else {
-    banner.style.display = 'block';
-    bannerMsg.textContent = 'Welcome to the Chamber of Commerce!';
+    banner.style.display = 'none';
 }
