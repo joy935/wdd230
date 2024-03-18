f6f4191810e91470cbddac2747f01220f65be66a
joy935 <tupaiv@gmail.com>
2024-03-18 16:48:24 +0100

    added blank attribute

--- chamber/scripts/getdirectory.js
+++ chamber/scripts/getdirectory.js
@@ -33,6 +33,7 @@ const displayMembers = (members) => {
         image.setAttribute('loading', 'lazy');
         image.setAttribute('width', '300');
         image.setAttribute('height', 'auto');
+        image.setAttribute('target', '_blank');
         name.textContent = `${member.name}`;
         address.textContent = `${member.address}`;
         phone.textContent = `${member.phone}`;
