ba37e6a67a2671e339298e0f64705f6ee8ea2b0c
joy935 <tupaiv@gmail.com>
2024-03-21 20:25:18 +0100

    removed old f

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -4,35 +4,9 @@ const boxevent = document.querySelector('.events-box');
 const contacts = document.querySelectorAll('.contact');
 const memberships = document.querySelectorAll('.memberships');
 
-// check for dark mode preference
-// darkMode.addEventListener('change', () => {
-//     // if dark mode is enabled
-//     if (darkMode.checked) {
-// 		body.classList.add('dark-style');
-//         memberships.forEach(membership => {
-//             membership.style.backgroundColor = '#333333';
-//         });
-//         boxevent.style.background = '#333333';
-//         contacts.forEach(contact => {
-//             contact.style.color = 'white';
-//         });
-//     }
-//     // if dark mode is disabled
-//     else {
-//         body.classList.remove('dark-style');
-//         memberships.forEach(membership => {
-//             membership.style.backgroundColor = '#E9F1EE';
-//         });
-//         boxevent.style.background = '#E9F1EE';
-//         contacts.forEach(contact => {
-//             contact.style.color = '#274F32';
-//         });
-//     }
-// });
-
-// Function to toggle dark mode
+// function to toggle dark mode
 function toggleDarkMode() {
-    // Check if the elements exist before attempting to apply changes
+    // check if the elements exist before attempting to apply changes
     if (body && boxevent && contacts && memberships) {
         // if dark mode is enabled
         if (darkMode.checked) {
@@ -64,5 +38,5 @@ if (darkMode) {
     darkMode.addEventListener('change', toggleDarkMode);
 }
 
-// Call the function initially to apply dark mode settings if needed
+// call the function initially to apply dark mode settings if needed
 toggleDarkMode();
