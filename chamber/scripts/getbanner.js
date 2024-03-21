228d0d9732b47058537f12255ebff2f52a5b9755
joy935 <tupaiv@gmail.com>
2024-03-21 20:33:40 +0100

    changed cdt banner

--- chamber/scripts/getbanner.js
+++ chamber/scripts/getbanner.js
@@ -5,7 +5,7 @@ const currentDate = new Date();
 const currentDay = currentDate.getDay();
 
 // if it's Monday, Tuesday, or Wednesday display the banner
-if (currentDay === 1 || currentDay == 2 || currentDay === 4) {
+if (currentDay === 1 || currentDay == 2 || currentDay === 3) {
   banner.style.display = 'block';
   bannerMsg.textContent = `Save the Date: The Chamber of Commerce will have a Meet and Greet on Wednesday at 7:00 p.m.`;
 // if it's any other day, hide the banner
