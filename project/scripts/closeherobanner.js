4815a29039dbd020af6d1895aacd4663a51b11bc
joy935 <tupaiv@gmail.com>
2024-04-01 14:35:12 +0200

    completed close banner f

---
+++ project/scripts/closeherobanner.js
@@ -0,0 +1,7 @@
+const closeHeroBannerBtn = document.getElementById('close-hero-banner');
+
+// Close the hero banner when clicked
+closeHeroBannerBtn.addEventListener('click', () => {
+  const heroBanner = document.getElementById('hero-banner');
+  heroBanner.style.display = 'none';
+});
