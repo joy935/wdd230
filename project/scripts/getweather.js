7589ad24cc7b8124cb4ebd13a296598df750807b
joy935 <tupaiv@gmail.com>
2024-04-02 13:55:21 +0200

    fixed url

--- project/scripts/getweather.js
+++ project/scripts/getweather.js
@@ -4,11 +4,11 @@ const currentIcon = document.getElementById('current-icon');
 const weatherDesc = document.querySelector('figcaption');
 const tomorrowTemp = document.getElementById('tomorrow-temp');
 
-const url = "https://api.openweathermap.org/data/2.5/forecast?lat=20.51&lon=-86.95&units=imperial&&appid=710bb94f8ac1a0695f7197b282e4ba48";
+const mltDays = "https://api.openweathermap.org/data/2.5/forecast?lat=20.51&lon=-86.95&units=imperial&&appid=710bb94f8ac1a0695f7197b282e4ba48";
 
 const getWeather = async () => {
     try {
-        const response = await fetch(url);
+        const response = await fetch(mltDays);
         if (response.ok) {
             const data = await response.json()
             console.log(data)
@@ -20,3 +20,5 @@ const getWeather = async () => {
         console.error("Error fetching data: ", error)
     }
 };
+
+getWeather();
