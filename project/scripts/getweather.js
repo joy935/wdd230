85215062e80245eeeea39023142fb4ad026f6254
joy935 <tupaiv@gmail.com>
2024-04-03 13:28:58 +0200

    completed weather icon

--- project/scripts/getweather.js
+++ project/scripts/getweather.js
@@ -2,6 +2,7 @@ const currentTemp = document.getElementById('current-temp');
 const currentHumidity = document.getElementById('current-humidity');
 const currentIcon = document.getElementById('current-icon');
 const weatherDesc = document.querySelector('figcaption');
+
 const tomorrowTemp = document.getElementById('tomorrow-temp');
 
 const mltDays = "https://api.openweathermap.org/data/2.5/forecast?lat=20.51&lon=-86.95&units=imperial&&appid=710bb94f8ac1a0695f7197b282e4ba48";
@@ -35,15 +36,15 @@ function displayWeather(data) {
         let desc = data.list[0].weather[0].description;
         let icon = data.list[0].weather[0].icon;
 
-        currentTemp.innerHTML = `${round(temp)}&deg;F`;
-        currentHumidity.innerHTML = `${humidity}%`;
+        currentTemp.innerHTML = `Temperature ${round(temp)}&deg;F`;
+        currentHumidity.innerHTML = `Humidity ${humidity}%`;
         weatherDesc.innerHTML = `${capitalize(desc)}`;
         currentIcon.setAttribute('alt', desc);
         currentIcon.setAttribute('loading', 'lazy');
         currentIcon.src = `https://openweathermap.org/img/w/${icon}.png`;
 
         let tempTomorrow = data.list[8].main.temp;
-        tomorrowTemp.innerHTML = `${roundToWhole(tempTomorrow)}&deg;F`;
+        tomorrowTemp.innerHTML = `Temperature ${round(tempTomorrow)}&deg;F`;
     }
     else {
         console.error("Temperature: N/A");
