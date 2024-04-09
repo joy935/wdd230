9e7a87a532d81352d5d8e99bd4f7b0d4950a7373
joy935 <tupaiv@gmail.com>
2024-04-09 19:09:48 +0200

    added dark mode hp

--- project/scripts/getdarkmode.js
+++ project/scripts/getdarkmode.js
@@ -1,17 +1,31 @@
 const darkMode = document.querySelector('.switch input[type="checkbox"]');
 const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+const banner = document.querySelector('#hero-banner');
+const reservemsg = document.querySelector('.reserve-msg');
+const specialoffer = document.querySelector('#special-offer');
 
 // function to toggle dark mode
 function activateDarkMode() {
     // check if the elements exist before attempting to apply changes
-    if (body && boxevent && contacts && memberships) {
+    if (body) {
         // if dark mode is enabled
         if (darkMode.checked) {
             body.classList.add('dark-style');
+            h1.style.color = '#14160C';
+            banner.style.color = '#14160C';
+            reservemsg.style.color = 'white';
+            reservemsg.style.backgroundColor = '#333333';
+            specialoffer.style.color = 'white';
+            specialoffer.style.backgroundColor = '#333333';
         }
         // if dark mode is disabled
         else {
             body.classList.remove('dark-style');
+            reservemsg.style.color = '#14160C';
+            reservemsg.style.backgroundColor = '#E9F1EE';
+            specialoffer.style.color = '#14160C';
+            specialoffer.style.backgroundColor = '#F1BA53';
             }
         }
     }
