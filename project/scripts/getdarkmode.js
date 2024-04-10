70ff8e519445b40a51581aa638d76dfb513e757a
joy935 <tupaiv@gmail.com>
2024-04-10 11:51:08 +0200

    fixed dark mode

--- project/scripts/getdarkmode.js
+++ project/scripts/getdarkmode.js
@@ -4,7 +4,6 @@ const h1 = document.querySelector('h1');
 const banner = document.querySelector('#hero-banner');
 const reservemsg = document.querySelector('.reserve-msg');
 const specialoffer = document.querySelector('#special-offer');
-const tbodyOddRows = document.querySelectorAll('tbody tr:nth-child(odd)');
 
 // function to toggle dark mode
 function activateDarkMode() {
@@ -19,9 +18,6 @@ function activateDarkMode() {
             reservemsg.style.backgroundColor = '#333333';
             specialoffer.style.color = 'white';
             specialoffer.style.backgroundColor = '#333333';
-            tbodyOddRows.forEach(row => {
-                row.style.backgroundColor = '#333333';
-            });
         }
         // if dark mode is disabled
         else {
